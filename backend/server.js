4dcfb9f6e1a2343aee476f631380960f1a8a97d0
vish-vishnu <vishnua2523@gmail.com>
2025-10-12 22:58:10 +0530

    Updated code

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const cors = require("cors");
 const app= express();
 app.use(cors());
 app.use(express.json());
-const PORT=3000;
+const PORT=process.env.PORT || 3000;
 
 const CREDENTIALS = JSON.parse(fs.readFileSync("credentials.json"));
 
